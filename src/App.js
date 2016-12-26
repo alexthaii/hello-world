3953f33e496f1d7188059bc57839520c4353ec67
alexthai <axt.421@gmail.com>
2016-12-25 20:12:04 -0500

    Get Star Data from API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+//
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import React from 'react';
+import Request from 'superagent';
+import _ from 'lodash';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount()
+  {
+    var url = "http://www.omdbapi.com/?s=star&r=json";
+    Request.get(url).then((response) =>
+    {
+      this.setState({
+            movies: response.body.Search,
+            total: response.body.Results
+        });
+    });
+  }
+
+  componentWillUpdate(nextProps, nextState)
+  {
+
+  }
+
+  render()
+  {
+    var movies = _.map(this.state.movies, (movie) => {
+      return <li>{movie.Title}</li>
+    });
+    return <div><input ref="textBox" type="text" />
+    <ul>{movies}</ul>
+    </div>
   }
 }
 
-export default App;
+ export default App;
